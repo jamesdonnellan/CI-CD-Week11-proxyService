54027e7a53289b5b7d2e8c18970af80d83ec60cf
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-12-03 15:53:00 +0000

    Crated New Project - Part B - Proxy Service

---
+++ src/main/java/ie/atu/lab6/proxyservice/ProxyServiceApplication.java
@@ -0,0 +1,16 @@
+package ie.atu.lab6.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ProxyServiceApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProxyServiceApplication.class, args);
+    }
+}
+
