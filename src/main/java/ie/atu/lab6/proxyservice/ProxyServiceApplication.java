2721529a8771a88bcef9f4395c8eea6768a0f33e
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-12-15 14:03:19 +0000

    Completed step B4 - Added ProxyController

--- src/main/java/ie/atu/lab6/proxyservice/ProxyServiceApplication.java
+++ src/main/java/ie/atu/lab6/proxyservice/ProxyServiceApplication.java
@@ -1,4 +1,4 @@
-package ie.atu.lab6.proxyservice;
+package java.ie.atu.lab6.proxyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
